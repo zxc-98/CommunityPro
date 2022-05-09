092a65b64d01a4ba74f9f3f41907db2d24606b5a
zxc_98 <1036795393@qq.com>
2022-05-09 21:22:27 +0800

    2022.5.9

--- src/main/java/com/zxc/community/service/UserService.java
+++ src/main/java/com/zxc/community/service/UserService.java
@@ -163,4 +163,14 @@ public class UserService implements CommunityConstant {
         loginTicketMapper.updateStatus(ticket, 1);
     }
 
+
+    public LoginTicket findLoginTicket(String ticket) {
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+
+
+    public int updateHeader(int userId, String headUrl) {
+        return userMapper.updateHeader(userId, headUrl);
+    }
+
 }
