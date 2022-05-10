1fe5ac7071a2dfcedb964ac732b61a3e690e230b
zxc_98 <1036795393@qq.com>
2022-05-10 20:41:10 +0800

    2022.5.10 使用注解+拦截器-->拦截登录才能实现的功能

---
+++ src/main/java/com/zxc/community/controller/interceptor/LoginRequiredInterceptor.java
@@ -0,0 +1,36 @@
+package com.zxc.community.controller.interceptor;
+
+import com.zxc.community.annotation.LoginRequired;
+import com.zxc.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+@Component
+public class LoginRequiredInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            // 获取到拦截方法
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Method method = handlerMethod.getMethod();
+            // 获取带有注解的拦截器方法 拦截
+            LoginRequired loginRequired = method.getAnnotation(LoginRequired.class);
+            if (loginRequired != null && hostHolder.getUser() == null) {
+                response.sendRedirect(request.getContextPath() + "/login");
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
