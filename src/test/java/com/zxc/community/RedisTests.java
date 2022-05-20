cfd29535d170821816cdb9069df75bd844c4bf34
zxc_98 <1036795393@qq.com>
2022-05-20 17:05:07 +0800

    2022.5.19 redis 实现点赞功能

---
+++ src/test/java/com/zxc/community/RedisTests.java
@@ -0,0 +1,63 @@
+package com.zxc.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testString() {
+        String key = "test:count";
+        redisTemplate.opsForValue().set(key, 1);
+
+        System.out.println(redisTemplate.opsForValue().get(key));
+        System.out.println(redisTemplate.opsForValue().increment(key));
+        System.out.println(redisTemplate.opsForValue().decrement(key));
+    }
+
+    @Test
+    public void testHashes() {
+        String redisKey = "test:user";
+
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "username", "zhangsan");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey,"id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey,"username"));
+    }
+
+    //
+    @Test
+    public void testTransactional() {
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations redisOperations) throws DataAccessException {
+                String redisKey = "test:tx";
+                redisOperations.multi();//start
+
+                redisOperations.opsForSet().add(redisKey, "zhangsan");
+                redisOperations.opsForSet().add(redisKey, "lisi");
+
+                System.out.println(redisOperations.opsForSet().members(redisKey));//null 事务内部查询无效
+
+                return redisOperations.exec();//tijiao
+            }
+        });
+
+        System.out.println(obj);
+    }
+}
